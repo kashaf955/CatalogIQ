15ed2813532207d6de2f3f9133a427ef81e604f1
kashaf955 <kashaftariq496@gmail.com>
2026-09-14 15:57:29 +0500

    Add header Compnent

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const Header = () => {
-    return (
-        <div className="flex justify-between items-center p-4 text-white">
-            <div className="flex items-center gap-4 ">
-                <div className="w-20 h-20">
-                    <Link to="/">
-                        <img src={logo} alt="logo" className="w-full h-full object-cover" />
-                    </Link>
-                </div>
-            </div>
-            <div className="flex items-center gap-4">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-
-            </div>
+  return (
+    <div className="flex justify-between items-center p-4 text-white">
+      <div className="flex items-center gap-4 ">
+        <div className="w-32 h-32">
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-full h-full object-cover" />
+          </Link>
         </div>
-    )
-}
+      </div>
+      <div className="flex items-center gap-10 text-xl ">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/pricing">Pricing</Link>
+        <Link to="/contact-us">Contact Us</Link>
+      </div>
+      <div className="flex items-center gap-4 ">
+        <button className="bg-indigo-500 text-white px-4 py-2 rounded-md">
+          <Link to="/login">Login</Link>
+        </button>
+        <button className="bg-indigo-500 text-white px-4 py-2 rounded-md">
+          <Link to="/register">Get Started</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
 export default Header;
